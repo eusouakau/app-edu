0270a182154c55ad86d4a4e680832318762ae157
Kauana Henriques <karoltvbl@gmail.com>
2022-06-19 20:48:03 -0300

    refatora endpoints e conexao com db

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,41 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
+const authMiddleware = require('./middlewares/auth');
+const userRoutes = require('./routes/user');
+const contentRoutes = require('./routes/content');
+const schoolClassRoutes = require('./routes/schoolClass');
+const schoolDisciplineRoutes = require('./routes/schoolDiscipline');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-require('./app/controllers/index')(app);
+app.use('/user', userRoutes);
+app.use('/content', contentRoutes);
+app.use('/schoolClass', schoolClassRoutes);
+app.use('/schoolDiscipline', schoolDisciplineRoutes);
 
-app.listen(3050, console.log('Servidor rodando na porta 3050'));
+app.use(authMiddleware);
+
+app.get('/', (req, res) => {
+    res.json({ ok: true , user: req.userId });
+});
+
+
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
+const SERVER_PORT = process.env.SERVER_PORT;
+
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@teste.vh0nl.mongodb.net/?retryWrites=true&w=majority`)
+.then(()=> {
+    console.log('Banco de dados conectado...');
+    app.listen(SERVER_PORT, () => {
+        console.log('Servidor rodando na porta 3050...');
+    });
+})
+.catch((err) => {
+    console.log(err);
+})
