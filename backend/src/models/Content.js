b59e6b4f0fb02726be3ac03f04bd771876e3ff62
Kauana Henriques <karoltvbl@gmail.com>
2022-06-19 22:01:56 -0300

    fix: modifica propriedade

--- backend/src/models/Content.js
+++ backend/src/models/Content.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
-const SchoolDiscipline = require('./SchoolDiscipline');
 
 
 const ContentSchema = new mongoose.Schema({
     title: { type: String, require: true },
     description: { type: String, required: true },
     link: { type: String, required: true },
-    schoolDiscipline: { type: SchoolDiscipline, required: true },
+    schoolDiscipline: { type: String, required: true },
     createdAt: { type: Date, default: Date.now  }
 });
 
