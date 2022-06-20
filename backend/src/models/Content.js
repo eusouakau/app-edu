a61117b0ae3c908762985fb56538c6cc35883d96
Kauana Henriques <karoltvbl@gmail.com>
2022-06-19 21:28:50 -0300

    fix: adiciona data de  riacao ao conteúdo, como solicitado nas avaliações

--- backend/src/models/Content.js
+++ backend/src/models/Content.js
@@ -6,7 +6,8 @@ const ContentSchema = new mongoose.Schema({
     title: { type: String, require: true },
     description: { type: String, required: true },
     link: { type: String, required: true },
-    schoolDiscipline: { type: SchoolDiscipline, required: true }
+    schoolDiscipline: { type: SchoolDiscipline, required: true },
+    createdAt: { type: Date, default: Date.now  }
 });
 
 
