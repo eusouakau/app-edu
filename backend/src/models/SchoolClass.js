b82e5b188c39d8c77817cf62c501ff8c0506ce57
Kauana Henriques <karoltvbl@gmail.com>
2022-06-21 23:33:33 -0300

    fix: padronização e ajustes para montar obj corretamente

--- backend/src/models/SchoolClass.js
+++ backend/src/models/SchoolClass.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 //const User = require('./User');
 
 
-const SchoolClassSchema = new mongoose.Schema({
+const SchoolClassSchema = new Schema({
     name: { type: String, require: true },
-  //  students: { type: [User], required: false },
+    students: { type: [Schema.Types.ObjectId], required: false },
     school: { type: String, required: true, },
     grade: { type: String, required: true },
 });
